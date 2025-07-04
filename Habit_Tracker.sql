208cb729e5df764bc18096e27f130576438977b2
Samy <samy.bakiri.cs@gmail.com>
2025-07-04 08:46:29 +0100

    update the DB structure

--- Habit_Tracker.sql
+++ Habit_Tracker.sql
@@ -9,7 +9,6 @@ CREATE TABLE Users (
     Email VARCHAR(100) NOT NULL UNIQUE,
     Password VARCHAR(100) NOT NULL
 );
-
 CREATE TABLE Habits(
     Habit_Id INT AUTO_INCREMENT PRIMARY KEY,
     Name VARCHAR(100) NOT NULL,
@@ -19,22 +18,23 @@ CREATE TABLE Habits(
     End_Time TIME,
     Importance ENUM('Nomral','Important','Crucial'),
     Frequency INT ,  
-    User_Id int FOREIGN KEY REFERENCES Users(User_Id),
+    User_Id int ,
+    FOREIGN KEY (User_Id) REFERENCES Users(User_Id)
 );
-
-
 CREATE TABLE Habit_Logs(
-    Logs_Id INT AUTO_INCREMENT NOT NULL,
+    Logs_Id INT AUTO_INCREMENT PRIMARY KEY,
     Status ENUM('Done', 'Missed'),
     Note TEXT,
     Mood TEXT,
     Date DATE,
     Time TIME,
-    Habit_Id FOREIGN KEY Habits(Habit_Id),
-)
+    Habit_Id INT,
+    FOREIGN KEY (Habit_Id) REFERENCES Habits(Habit_Id)
+);
 CREATE TABLE Journal_Note(
-    Journal_Id INT AUTO_INCREMENT NOT NULL,
+    Journal_Id INT AUTO_INCREMENT PRIMARY KEY,
     Date DATE,
     Journal TEXT,
-    User_Id FOREIGN KEY Users(User_Id)
+    User_Id INT,
+    FOREIGN KEY (User_Id) REFERENCES Users(User_Id)
 );
