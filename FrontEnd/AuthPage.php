0ff6a0b370973fec1964e98ccf23647183a95171
Samy <samy.bakiri.cs@gmail.com>
2025-07-23 14:40:10 +0100

    create dashboard structure

--- FrontEnd/AuthPage.php
+++ FrontEnd/AuthPage.php
@@ -4,10 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HabTrack - Log in or sign up</title>
-    <link rel="stylesheet" href="Style/Main.css">
+    <link rel="stylesheet" href="Style/Auth_Page_Style.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"
-/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"/>
 
 
 </head>
